d7a4c2eee7007dbe5ffba3fc53cdbae79bc4140d
Huy Phan <huy.phan@be.com.vn>
2024-07-19 23:12:20 +0700

    add 2FA verify & fix query device logic

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -71,45 +71,57 @@ exports.handleLogin = async (req, res) => {
         const { email, password, deviceId } = req.body;
         const ipAddress = req.ip;
 
-        console.log("Device ID: ", deviceId);
         // Kiem tra user
         const user = await User.findOne({ where: { email } });
-
         if (!user) {
             return res.redirect('/login');
         }
 
         const match = await bcrypt.compare(password, user.password);
-
         if (!match) {
             console.log("Authentication failed");
             return res.redirect('/login');
         }
 
+        let device = await Device.findOne({ where: { device: deviceId } });
+        if (!device) {
+            device = await Device.create({
+                device: deviceId
+            });
+        }
+
+        let userDevice = await UserDevice.findOne({ where : { userID: user.id, deviceID: device.id } });
+        
+
+
+
         // Lay danh sach thiet bi cua User da duoc xac thuc (luu o DB)
-        const userDevices = await UserDevice.findAll({where: {userID: user.id}});
-        const deviceIds = userDevices.map(ud => ud.deviceID);
+        // const userDevices = await UserDevice.findAll({where: {userID: user.id}});
+        // const deviceIds = userDevices.map(ud => ud.deviceID);
 
-        // // Kiem tra thiet bi moi co nam trong danh sach khong
-        const device = await Device.findOne({ where: { device: deviceId } });
-        const deviceExists = device && deviceIds.includes(device.id);
+        // // // Kiem tra thiet bi moi co nam trong danh sach khong
+        // const device = await Device.findOne({ where: { device: deviceId } });
+        // const deviceExists = device && deviceIds.includes(device.id);
 
-        // // Kiem tra dia chi IP cua user
-        const userIps = await UserIPAddress.findAll({ where: { userID: user.id } });
-        const ipIds = userIps.map(ui => ui.ipAddressID);
+        // // // Kiem tra dia chi IP cua user
+        // const userIps = await UserIPAddress.findAll({ where: { userID: user.id } });
+        // const ipIds = userIps.map(ui => ui.ipAddressID);
 
-        const ip = await IPAddress.findOne({ where: { ipAddress: ipAddress } });
-        const ipExists = ip && ipIds.includes(ip.id);
+        // const ip = await IPAddress.findOne({ where: { ipAddress: ipAddress } });
+        // const ipExists = ip && ipIds.includes(ip.id);
 
-        console.log(ipExists);
+        // console.log(ipExists);
 
-        if (!deviceExists) {
+        if (!userDevice) {
+            
+
             const pin = generatePIN();
             
             // Save the PIN for verification
             await Verification.create({
                 userID: user.id,
-                pin: pin
+                pin,
+                createdAt: new Date(),
             });
 
             const mailOptions = {
@@ -121,9 +133,14 @@ exports.handleLogin = async (req, res) => {
 
             await transporter.sendMail(mailOptions);
 
+            req.session.userId = user.id;
+            req.session.deviceID = device.id;
+            req.session.deviceId = deviceId;
+            req.session.ipAddress = ipAddress;
+
+
             // Redirect to verification page
-            // return res.redirect('/verify');
-            return res.redirect('/login');
+            return res.redirect('/OTP');
         }
 
         // If the device and IP are verified
@@ -156,23 +173,68 @@ function generatePIN() {
 
 exports.verifyPIN = async (req, res) => {
     try {
-        const { userId, pin } = req.body;
+        const { pin } = req.body;
+        const userId = req.session.userId;
+        const deviceId = req.session.deviceId;
+        const deviceID = req.session.deviceID;
+        const ipAddress = req.session.ipAddress;
 
-        const verification = await Verification.findOne({ where: { userID: userId, pin } });
-
-        if (!verification) {
-            return res.status(400).send('Invalid PIN');
+        console.log(userId);
+        if (!userId) {
+            return res.status(400).json({ error: 'User ID not found in session' });
         }
 
-        // If the PIN is correct, you can proceed with the login process
-        // Mark the device and IP as verified if needed
-        await UserDevice.create({ userID: userId, deviceID: verification.deviceID });
-        await UserIPAddress.create({ userID: userId, ipAddressID: verification.ipAddress });
+        const userIdStr = userId.toString();
+
+        const verification = await Verification.findOne({ where: { userID: userIdStr, pin } });
+
+        if (!verification) {
+            return res.status(400).json({ error: 'Invalid PIN' });
+        }
+
+        // If the PIN is correct, mark the device and IP as verified if needed
+        await UserDevice.create({ userID: userId, deviceID: deviceID });
 
         // Redirect to home
         return res.redirect('/home');
     } catch (error) {
-        console.error(error);
-        return res.status(500).send('Server error');
+        console.error('Error verifying PIN:', error);
+        return res.status(500).json({ error: 'An error occurred while verifying OTP. Please try again later.' });
     }
 };
+
+exports.resendPIN = async (req, res) => {
+    try {
+        const userId = req.session.userId;
+        if (!userId) {
+            return res.status(400).json({ error: 'User ID not found in session' });
+        }
+
+        const pin = generatePIN();
+
+        // Save the PIN for verification
+        await Verification.create({
+            userID: userId,
+            pin,
+            createdAt: new Date(),
+        });
+
+        const user = await User.findByPk(userId);
+
+        const mailOptions = {
+            from: 'huymasterpiece@gmail.com',
+            to: user.email,
+            subject: 'Verification PIN',
+            text: `A login attempt was made from a new device or IP address. Please verify by entering this PIN: ${pin}`
+        };
+
+        await transporter.sendMail(mailOptions);
+
+        return res.status(200).json({ message: 'PIN sent successfully' });
+
+    } catch (error) {
+        console.error('Error resending PIN:', error);
+        return res.status(500).json({ error: 'An error occurred while resending OTP. Please try again later.' });
+    }
+}
+
