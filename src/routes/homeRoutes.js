ec158f085c0e98d830c97c70bd7e6a846e7b9f45
BaoNinh <4legsmonster@gmail.com>
2024-07-20 11:03:59 +0700

    represent data in database in homepage + update Upload file to IPFS

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const homeController = require('../controllers/homeController');
-const authMiddleware = require('../middleware/authMiddleware');
+const controller = require("../controllers/homeController");
 
-router.get('/home', authMiddleware.isAuthenticated, homeController.home);
-router.get('/verify', homeController.verify);
+// const homeController = require('../controllers/homeController');
+// const authMiddleware = require('../middleware/authMiddleware');
+
+// router.get('/home', authMiddleware.isAuthenticated, homeController.home);
+// router.get('/verify', homeController.verify);
+
+router.get('/', controller.getHomePage);
+router.put('/', controller.upload, controller.handleUpload);
 
 module.exports = router;
