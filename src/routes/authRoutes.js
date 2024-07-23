586377136c22ea420cb8d3d1a6d81f917f12f503
socsungsuc <haisonqn2003@gmail.com>
2024-07-23 23:59:04 +0700

    alert error while login && register

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,7 +5,7 @@ const authController = require('../controllers/authController');
 router.get('/login', authController.login);
 router.post('/login', authController.handleLogin);
 router.get('/register', authController.register);
-router.post('/auth/register', authController.handleRegister);
+router.post('/register', authController.handleRegister);
 router.post('/auth/verify', authController.verifyPIN);
 router.post('/auth/resend', authController.resendPIN);
 router.post('/logout', authController.logout);
